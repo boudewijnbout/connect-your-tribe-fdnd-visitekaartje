208609b149b817ed83ae76becb278e8eb2d07db5
boudewijnbout <boudewijn_bout@outlook.com>
2022-02-09 17:02:27 +0100

    Added slide effect on video

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,19 @@
-// Fetch API Data
+const toggleBtn = document.querySelector('button');
+
+const backgroundVideo = document.querySelector('video');
+
+const left = document.querySelector('.left'),
+      right = document.querySelector('.right')
+
+toggleBtn.addEventListener('click', function() {
+
+    left.classList.add('hide');
+    right.classList.add('hide');
+
+
+    backgroundVideo.play();
+});
+
 fetch('https://tribe.api.fdnd.nl/v1/member')
-.then(res => res.json())
-.then(data => console.log(data));
+    .then(response => response.json())
+    .then(data => console.log(data));
