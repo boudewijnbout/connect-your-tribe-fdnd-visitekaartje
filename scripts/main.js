8446640f7739a015b70c2583795b165f4c56522f
boudewijnbout <boudewijn_bout@outlook.com>
2022-02-10 14:07:55 +0100

    Visite kaartje v1.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,37 @@
+// Background video + animated toggle layer
 const toggleBtn = document.querySelector('button');
 
 const backgroundVideo = document.querySelector('video');
 
-const left = document.querySelector('.left'),
-      right = document.querySelector('.right')
+const left = document.querySelector('.left-page-column'),
+    right = document.querySelector('.right-page-column')
 
-toggleBtn.addEventListener('click', function() {
+toggleBtn.addEventListener('click', function () {
 
     left.classList.add('hide');
     right.classList.add('hide');
 
-
     backgroundVideo.play();
 });
 
-fetch('https://tribe.api.fdnd.nl/v1/member')
-    .then(response => response.json())
-    .then(data => console.log(data));
+
+// Fetch the API data
+const memberId = 18;
+const apiUrl = 'https://tribe.api.fdnd.nl/v1/member';
+
+fetch(apiUrl)
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        const boudewijn = data.data.find(student => student.memberId === 18);
+
+        renderApiData(boudewijn);
+    })
+    .catch(function (error) {
+        console.log("Api can't connect");
+    });
+
+const renderApiData = (boudewijn) => {
+    document.getElementById('first-name').innerHTML = boudewijn.name;
+}
