8d5769ca2c7eaadd899852e37d10b6096dc243a6
boudewijnbout <boudewijn_bout@outlook.com>
2022-02-22 13:59:31 +0100

    Visitekaartje remake

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,21 @@
-// Background video + animated toggle layer
-const toggleBtn = document.querySelector('button');
-
-const backgroundVideo = document.querySelector('video');
-
-const left = document.querySelector('.left-page-column'),
-    right = document.querySelector('.right-page-column')
-
-toggleBtn.addEventListener('click', function () {
-
-    left.classList.add('hide');
-    right.classList.add('hide');
-
-    backgroundVideo.play();
-});
-
-
-// Fetch the API data
+// Fetch API data
 const memberId = 18;
-const apiUrl = 'https://tribe.api.fdnd.nl/v1/member';
+const apiUrl = "https://tribe.api.fdnd.nl/v1/member";
 
 fetch(apiUrl)
-    .then((res) => {
-        return res.json();
-    })
-    .then((data) => {
-        const boudewijn = data.data.find(student => student.memberId === 18);
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    const boudewijn = data.data.find((student) => student.memberId === 18);
 
-        renderApiData(boudewijn);
-    })
-    .catch(function (error) {
-        console.log("Api can't connect");
-    });
+    renderApiData(boudewijn);
+  })
+  .catch(function (error) {
+    console.log("Api can't connect");
+  });
 
+// Render API Data
 const renderApiData = (boudewijn) => {
-    document.getElementById('first-name').innerHTML = boudewijn.name;
-}
+  document.getElementById("name-title").innerHTML = `${boudewijn.name} ${boudewijn.surname}`;
+};
